1690c02fd858b54f8d38390cff362ba5cbc9172e
Eugene-TechnoNinja <technoninja@ukr.net>
2026-01-09 23:24:28 +0200

    delete trash

--- src/main.cpp
+++ src/main.cpp
@@ -34,26 +34,6 @@ int main(void) {
   parser.Parse(response.at("body"), Weather::CITIES,
                ui.GetActiveItemDropdownMenu());
 
-  // std::map<std::string, std::string> dataHeroCard = {
-  //     {"pic", "day_overcast"},
-  //     {"weather", "Overcast"},
-  //     {"city", "Kharkiv City"},
-  //     {"temp", "-10"},
-  // };
-
-  // std::vector<std::map<std::string, std::string>> dataHourlyCard = {
-  //     {{"time", "3PM"}, {"temp", "1"}},  {{"time", "4PM"}, {"temp", "-3"}},
-  //     {{"time", "5PM"}, {"temp", "3"}},  {{"time", "6PM"}, {"temp", "-4"}},
-  //     {{"time", "7PM"}, {"temp", "-2"}},
-  // };
-
-  // std::vector<std::map<std::string, std::string>> dataPredictions = {
-  //     {{"pic", "mini_cloud"}, {"title", "Cloud"}, {"result", "100%"}},
-  //     {{"pic", "mini_visibility"}, {"title", "Visibility"}, {"result", "100%"}},
-  //     {{"pic", "mini_water"}, {"title", "Humidity"}, {"result", "1%"}},
-  //     {{"pic", "mini_windy"}, {"title", "Wind speed"}, {"result", "110km/h"}},
-  // };
-
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(GetColor(Weather::palette.dark));
